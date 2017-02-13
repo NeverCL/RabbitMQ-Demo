86af57f10672f2b8541f145fa36cc635036fb638
Never、C <caolin>
2017-02-13 15:54:16 +0800

    fix  helloworld

--- RabbitMQ-Demo/Receive/Program.cs
+++ RabbitMQ-Demo/Receive/Program.cs
@@ -12,8 +12,13 @@ namespace Receive
     {
         static void Main(string[] args)
         {
+            HelloWorld();
+        }
+
+        private static void HelloWorld()
+        {
             var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
+            using (var connection = factory.CreateConnection()) // 与Producer Send配置一样
             {
                 using (var channel = connection.CreateModel())
                 {
@@ -23,13 +28,14 @@ namespace Receive
                                  autoDelete: false,
                                  arguments: null);
 
-                    var consumer = new EventingBasicConsumer(channel);
-                    consumer.Received += (model, ea) =>
+                    var consumer = new EventingBasicConsumer(channel);  // 创建Consumer
+                    consumer.Received += (model, ea) =>         // 通过回调函数异步推送我们的消息
                     {
                         var body = ea.Body;
                         var message = Encoding.UTF8.GetString(body);
                         Console.WriteLine(" [x] Received {0}", message);
                     };
+
                     channel.BasicConsume(queue: "hello",
                                          noAck: true,
                                          consumer: consumer);
--- RabbitMQ-Demo/Send/Program.cs
+++ RabbitMQ-Demo/Send/Program.cs
@@ -11,30 +11,39 @@ namespace Send
     {
         static void Main(string[] args)
         {
+            HelloWorld();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine(" Press [Ctrl + C] to exit.");
             var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
+            using (var connection = factory.CreateConnection()) // 创建与服务器的连接
             {
-                using (var channel = connection.CreateModel())
+                using (var channel = connection.CreateModel()) // 创建1个通道,大部分API在该类中
                 {
-                    channel.QueueDeclare(queue: "hello",
+                    channel.QueueDeclare(queue: "hello",    // 定义1个队列
                                 durable: false,
                                 exclusive: false,
                                 autoDelete: false,
                                 arguments: null);
 
-                    string message = "Hello World!";
-                    var body = Encoding.UTF8.GetBytes(message);
+                    while (true)
+                    {
+                        string message = "Hello World!" + DateTime.Now;
+                        var body = Encoding.UTF8.GetBytes(message);
 
-                    channel.BasicPublish(exchange: "",
-                                         routingKey: "hello",
-                                         basicProperties: null,
-                                         body: body);
-                    Console.WriteLine(" [x] Sent {0}", message);
+                        channel.BasicPublish(exchange: "",      // 发送消息到队列中
+                                             routingKey: "hello",
+                                             basicProperties: null,
+                                             body: body);       // 发送的是bytes 可以任意编码
+                        Console.WriteLine(" [x] Sent {0}", message);
+                        Console.ReadLine();
+                    }
+
                 }
             }
 
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
         }
     }
 }
