254ee32b30db5b78d02bbdc6f6d830dae87be335
曹麟 <caolin>
2017-03-19 17:18:49 +0800

    fix send + receive

--- RabbitMQ-Demo/Receive/Program.cs
+++ RabbitMQ-Demo/Receive/Program.cs
@@ -11,150 +11,39 @@ namespace Receive
 {
     class Program
     {
+        private static string hostName = "test.youlijinfu.com";
         static void Main(string[] args)
         {
-            Routing();
+            ReceiveMsg();
         }
 
-        /// <summary>
-        /// Routing
-        /// </summary>
-        private static void Routing()
+        private static void ReceiveMsg()
         {
-            string[] args;
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
+            var factory = new ConnectionFactory
             {
-                channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
-                var queueName = channel.QueueDeclare().QueueName;
-
-                args = Console.ReadLine().Split(' ');
-
-                foreach (var severity in args)
-                {
-                    channel.QueueBind(
-                        queue: queueName,
-                        exchange: "direct_logs",
-                        routingKey: severity);      // 绑定的路由键
-                }
-
-                Console.WriteLine(" [*] Waiting for messages.");
-
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    var body = ea.Body;
-                    var message = Encoding.UTF8.GetString(body);
-                    var routingKey = ea.RoutingKey;
-                    Console.WriteLine(" [x] Received '{0}':'{1}'",
-                        routingKey, message);
-                };
-                channel.BasicConsume(queue: queueName,
-                    noAck: true,
-                    consumer: consumer);
-
-                Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
-            }
-        }
-
-        /// <summary>
-        /// Subscribe订阅
-        /// </summary>
-        private static void PublishSubscribe()
-        {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                channel.ExchangeDeclare(exchange: "logs", type: "fanout");  //type 为内置 ExchangeType.Fanout
-
-                var queueName = channel.QueueDeclare().QueueName;   // 新建临时队列,提出自动删除该队列
-                channel.QueueBind(queue: queueName,                 // 绑定queue和exchange
-                                  exchange: "logs",
-                                  routingKey: "");
-
-                Console.WriteLine(" [*] Waiting for logs.");
-
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    var body = ea.Body;
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] {0}", message);
-                };
-                channel.BasicConsume(queue: queueName,
-                                     noAck: true,
-                                     consumer: consumer);
-
-                Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
-            }
-        }
-
-
-        /// <summary>
-        /// 消息确认 消息分配
-        /// </summary>
-        private static void WorkQueue()
-        {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
+                HostName = hostName,                // rabbit server
+                UserName = "admin",
+                Password = "admin",
+                Port = 5672,                        // Broker端口
+                VirtualHost = "/"                   // 虚拟Host,需提前配置
+            };
             using (var connection = factory.CreateConnection())
             {
                 using (var channel = connection.CreateModel())
                 {
-                    channel.QueueDeclare(queue: "workQueue",
-                                 durable: false,
-                                 exclusive: false,
-                                 autoDelete: false,
-                                 arguments: null);
-
-                    var consumer = new EventingBasicConsumer(channel);
-                    consumer.Received += (model, ea) =>
-                    {
-                        var body = ea.Body;
-                        var message = Encoding.UTF8.GetString(body);
-                        Console.WriteLine(" [x] Received {0}", message);
-                        Thread.Sleep(3000);
-                        Console.WriteLine(" [x] Done!");
-                        channel?.BasicAck(ea.DeliveryTag, false);   // 发送消息确认,队列收到确认才移除消息
-                    };
-                    channel.BasicQos(0, 1, false);              // 直到队列完成才接受新的,默认为轮流接受
-
-                    channel.BasicConsume(queue: "workQueue",
-                                         //noAck: true,         // Message acknowledgment 消息确认
-                                         consumer: consumer);
-
-                    Console.WriteLine(" Press [enter] to exit.");
-                    Console.ReadLine();
-                }
-            }
-        }
-
-        private static void HelloWorld()
-        {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection()) // 与Producer Send配置一样
-            {
-                using (var channel = connection.CreateModel())
-                {
-                    channel.QueueDeclare(queue: "hello",
-                                 durable: false,
-                                 exclusive: false,
-                                 autoDelete: false,
-                                 arguments: null);
-
                     var consumer = new EventingBasicConsumer(channel);  // 创建Consumer
                     consumer.Received += (model, ea) =>         // 通过回调函数异步推送我们的消息
                     {
                         var body = ea.Body;
                         var message = Encoding.UTF8.GetString(body);
+                        Thread.Sleep(1000);
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false); // 消息响应
                         Console.WriteLine(" [x] Received {0}", message);
                     };
 
+                    channel.BasicQos(0, 1, false);  // 设置perfetchCount=1 。这样就告诉RabbitMQ 不要在同一时间给一个工作者发送多于1个的消息
                     channel.BasicConsume(queue: "hello",
-                                         noAck: true,
+                                         noAck: false,      // 需要消息响应（Acknowledgments）机制
                                          consumer: consumer);
 
                     Console.WriteLine(" Press [enter] to exit.");
--- RabbitMQ-Demo/Send/Program.cs
+++ RabbitMQ-Demo/Send/Program.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Threading;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Framing.Impl;
 
 namespace Send
 {
@@ -14,7 +15,64 @@ namespace Send
 
         static void Main(string[] args)
         {
-            HelloWorld();
+            SendMsg();
+        }
+
+        private static void SendMsg()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = hostName,                // rabbit server
+                UserName = "admin",
+                Password = "admin",
+                Port = 5672,                        // Broker端口
+                VirtualHost = "/"                   // 虚拟Host,需提前配置
+            };
+
+            using (var connection = factory.CreateConnection()) // 创建与RabbitMQ服务器的连接
+            {
+                using (var channel = connection.CreateModel())  // 创建1个Channel(大部分API在该Channel中)
+                {
+                    // 定义1个队列,自动会和默认的exchange 做direct类型绑定
+                    channel.QueueDeclare(
+                        queue: "hello",                     // 队列名称
+                        durable: true,                      // 队列是否持久化
+                        exclusive: false,                   // 排他队列:如果一个队列被声明为排他队列，该队列仅对首次声明它的连接可见，并在连接断开时自动删除。(活动在一次连接内)
+                        autoDelete: false,                  // 自动删除:当最后一个消费者取消订阅时，队列自动删除。如果您需要仅由一个使用者使用的临时队列，请将自动删除与排除。当消费者断​​开连接时，队列将被删除。(至少消费者能连一次)
+                        arguments: null);
+
+                    var randomQueue = channel.QueueDeclare();                   // 定义随机的队列 该队列为临时队列(排他队列 + 自动删除)
+
+
+                    // 定义Exchange(一般而言,不需要定义exchange,rabbitmq默认创建了所有类型的exchange)
+                    //channel.ExchangeDeclare("direct-demo", ExchangeType.Direct);    // 定义direct exchange
+                    //channel.ExchangeDeclare("fannout-demo", ExchangeType.Fanout);   // 定义fanout exchange
+                    //channel.ExchangeDeclare("topic-demo", ExchangeType.Topic);      // 定义fanout exchange
+
+
+                    // 定义queue exchange key 关系(在某些业务场景下,会使用该关系做路由功能)
+                    //channel.QueueBind(queue: "hello", exchange: "amq.direct", routingKey: "hello"); // 默认绑定的关系和该行代码效果一样
+                    //channel.QueueBind("hello", "amq.fanout", "hello");                              // 该类型下的routingKey 实际不需要
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    while (true)
+                    {
+                        string message = "Hello World!" + DateTime.Now;
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        // 发送消息到队列中
+                        channel.BasicPublish(
+                            exchange: string.Empty,         // 传递为Empty的时候,通过	`(AMQP default)`传递
+                            routingKey: "hello",            // routing key 与 queuebind中的binding key对应
+                            basicProperties: properties,    // 消息header
+                            body: body);                    // 消息body:发送的是bytes 可以任意编码
+
+                        Console.WriteLine(" [x] Sent {0}", message);
+                    }
+                }
+            }
         }
 
         /// <summary>
@@ -22,7 +80,7 @@ namespace Send
         /// </summary>
         private static void Routing()
         {
-            var factory = new ConnectionFactory() {HostName = "localhost"};
+            var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
@@ -50,7 +108,7 @@ namespace Send
         private static void PublishSubscribe()
         {
             Console.WriteLine(" Press [Ctrl + C] to exit.");
-            var factory = new ConnectionFactory() {HostName = "localhost"};
+            var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
@@ -75,7 +133,7 @@ namespace Send
         /// </summary>
         private static void WorkQueue()
         {
-            var factory = new ConnectionFactory() {HostName = "localhost"};
+            var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
             {
                 using (var channel = connection.CreateModel())
@@ -116,25 +174,30 @@ namespace Send
                 VirtualHost = "/"                   // 虚拟Host,需提前配置
             };
 
-            using (var connection = factory.CreateConnection()) // 创建与服务器的连接
+            using (var connection = factory.CreateConnection()) // 创建与RabbitMQ服务器的连接
             {
-                using (var channel = connection.CreateModel()) // 创建1个通道,大部分API在该channel中
+                using (var channel = connection.CreateModel())  // 创建1个Channel(大部分API在该Channel中)
                 {
-                    // 定义1个队列,默认会和默认的exchange 做direct绑定
-                    channel.QueueDeclare(queue: "hello",   // 队列名称
-                        durable: true,                      // 是否持久化
-                        exclusive: false,                   // 排他队列:如果一个队列被声明为排他队列，该队列仅对首次声明它的连接可见，并在连接断开时自动删除。
-                        autoDelete: false,                  // 自动删除:当最后一个消费者取消订阅时，队列自动删除。如果您需要仅由一个使用者使用的临时队列，请将自动删除与排除。当消费者断​​开连接时，队列将被删除。
+                    // 定义1个队列,自动会和默认的exchange 做direct类型绑定
+                    channel.QueueDeclare(
+                        queue: "hello",                     // 队列名称
+                        durable: true,                      // 队列是否持久化
+                        exclusive: false,                   // 排他队列:如果一个队列被声明为排他队列，该队列仅对首次声明它的连接可见，并在连接断开时自动删除。(活动在一次连接内)
+                        autoDelete: false,                  // 自动删除:当最后一个消费者取消订阅时，队列自动删除。如果您需要仅由一个使用者使用的临时队列，请将自动删除与排除。当消费者断​​开连接时，队列将被删除。(至少消费者能连一次)
                         arguments: null);
 
-                    channel.ExchangeDeclare("test",ExchangeType.Direct);    // 定义exchange
-                    channel.ExchangeDeclare("puball",ExchangeType.Fanout);    // 定义exchange
+                    var randomQueue = channel.QueueDeclare();                   // 定义随机的队列 该队列为临时队列(排他队列 + 自动删除)
 
-                    var queueName = channel.QueueDeclare().QueueName;   // 定义随机的队列
 
-                    channel.QueueBind("hello","test","hello");
-                    channel.QueueBind("hello","puball","hello");
-                    channel.QueueBind("hello2", "puball", "hello1");
+                    // 定义Exchange(一般而言,不需要定义exchange,rabbitmq默认创建了所有类型的exchange)
+                    //channel.ExchangeDeclare("direct-demo", ExchangeType.Direct);    // 定义direct exchange
+                    //channel.ExchangeDeclare("fannout-demo", ExchangeType.Fanout);   // 定义fanout exchange
+                    //channel.ExchangeDeclare("topic-demo", ExchangeType.Topic);      // 定义fanout exchange
+
+
+                    // 定义queue exchange key 关系(在某些业务场景下,会使用该关系做路由功能)
+                    //channel.QueueBind(queue: "hello", exchange: "amq.direct", routingKey: "hello"); // 默认绑定的关系和该行代码效果一样
+                    //channel.QueueBind("hello", "amq.fanout", "hello");                              // 该类型下的routingKey 实际不需要
 
                     var properties = channel.CreateBasicProperties();
                     properties.Persistent = true;
@@ -145,13 +208,13 @@ namespace Send
                         var body = Encoding.UTF8.GetBytes(message);
 
                         // 发送消息到队列中
-                        channel.BasicPublish(exchange: "puball", // 传递为Empty的时候,通过	`(AMQP default)`传递
+                        channel.BasicPublish(
+                            exchange: string.Empty,         // 传递为Empty的时候,通过	`(AMQP default)`传递
                             routingKey: "hello",            // routing key 与 queuebind中的binding key对应
                             basicProperties: properties,    // 消息header
                             body: body);                    // 消息body:发送的是bytes 可以任意编码
 
                         Console.WriteLine(" [x] Sent {0}", message);
-                        Thread.Sleep(1000);
                     }
                 }
             }
